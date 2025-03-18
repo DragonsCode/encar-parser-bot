730be281330c0e9671304f26f15129204112587b
DragonsCode <dilimmurod@gmail.com>
2025-03-18 21:14:20 +0500

    DB fix

--- database/db_session.py
+++ database/db_session.py
@@ -13,7 +13,7 @@ async def global_init(user, password, host, port, dbname, delete_db=False):
     if __factory:
         return
     # postgresql+asyncpg для postgres и mysql+aiomysql для mysql
-    conn_str = f'mysql+aiomysql://{user}:{password}@{host}:{port}/{dbname}'
+    conn_str = f'postgresql+asyncpg://{user}:{password}@{host}:{port}/{dbname}'
     print(f"Подключение к базе данных по адресу {conn_str}")
 
     engine = create_async_engine(conn_str, pool_pre_ping=True)
