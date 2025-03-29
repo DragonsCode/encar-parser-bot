a14b286ac66bbe893f1e74c64b138c3f5e943bdb
DragonsCode <dilimmurod@gmail.com>
2025-03-29 10:27:03 +0500

    Bug fix

--- api/routers/subscriptions.py
+++ api/routers/subscriptions.py
@@ -15,7 +15,7 @@ async def create_subscription(sub_data: SubscriptionCreate, is_admin: bool = Dep
         return await db.get_subscription_by_id(sub.id)
 
 @router.get("/{user_id}", response_model=SubscriptionResponse)
-async def get_subscription(auth_user_id: int = Depends(telegram_auth)):
+async def get_subscription(user_id: int, auth_user_id: int = Depends(telegram_auth)):
     """
     Depends on telegram_auth
     """
@@ -23,7 +23,8 @@ async def get_subscription(auth_user_id: int = Depends(telegram_auth)):
         sub = await db.get_subscription_by_user(auth_user_id)
         if not sub:
             raise HTTPException(status_code=404, detail="Подписка не найдена")
-        return sub
+        # Преобразуем объект SQLAlchemy в Pydantic-модель
+        return SubscriptionResponse.model_validate(sub)
 
 @router.post("/edit", response_model=SubscriptionResponse)
 async def edit_subscription(sub_data: SubscriptionEdit, is_admin: bool = Depends(admin_auth)):
