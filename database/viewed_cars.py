a6f62ffc2277cc85879c19691d380b43fa95855d
DragonsCode <dilimmurod@gmail.com>
2025-04-02 15:57:28 +0500

    Bug fix

--- database/viewed_cars.py
+++ database/viewed_cars.py
@@ -7,7 +7,7 @@ class ViewedCars(SqlAlchemyBase):
     id = Column(BigInteger, primary_key=True, autoincrement=True)
     user_id = Column(BigInteger, ForeignKey("users.id"))
     filter_id = Column(BigInteger, ForeignKey("filters.id"))
-    car_id = Column(BigInteger, ForeignKey("car.id"), ondelete="CASCADE")
+    car_id = Column(BigInteger)
     viewed_dttm = Column(DateTime, default=datetime.now)
 
     def __repr__(self):
