bd67c54c54f783a7750fd79a5bc46bc8d246a733
DragonsCode <dilimmurod@gmail.com>
2025-04-07 22:33:04 +0500

    Remade parser

--- tasks/__init__.py
+++ tasks/__init__.py
@@ -1,5 +1,5 @@
 from .parser import run_parser_periodically
 from .subscriptions import check_subscriptions
-from .translate import run_translation
+# from .translate import run_translation
 
-__all__ = ['run_parser_periodically', 'check_subscriptions', 'run_translation']
+__all__ = ['run_parser_periodically', 'check_subscriptions']
